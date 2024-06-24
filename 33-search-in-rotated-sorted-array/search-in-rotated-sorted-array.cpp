453372bb935c15726ca2ef83c7f0c1f7346dd772
HimanshuJR <98231288+HimanshuJR@users.noreply.github.com>
2024-06-24 18:22:21 +0530

    Time: 3 ms (59.72%) | Memory: 13.6 MB (6.23%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,34 +1,95 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int start = 0;
-        int end = nums.size() - 1;
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2; // Calculate the midpoint
-
-            if (nums[mid] == target) {
-                return mid; // Target found
+    
+    int binarysearch(vector<int> nums,int low,int high,int target)
+    {
+        
+          int mid;
+        
+        while(high>=low)
+        {
+            mid= low+(high-low)/2;
+            
+            if(nums[mid]==target)
+            {
+                return mid;
+            }else if(nums[mid]>target)
+            {
+                high=mid-1;
+            }else{
+                low=mid+1;
             }
-
-            // Determine which part is sorted
-            if (nums[start] <= nums[mid]) {
-                // Left part is sorted
-                if (nums[start] <= target && target < nums[mid]) {
-                    end = mid - 1; // Target is in the left part
-                } else {
-                    start = mid + 1; // Target is in the right part
-                }
-            } else {
-                // Right part is sorted
-                if (nums[mid] < target && target <= nums[end]) {
-                    start = mid + 1; // Target is in the right part
-                } else {
-                    end = mid - 1; // Target is in the left part
-                }
-            }
+            
+            
         }
-
-        return -1; // Target not found
+        
+        
+        
+     return -1;   
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    int search(vector<int>& nums, int target) {
+        
+        int pivot=-1;
+        
+        int low=0;
+        int high=nums.size()-1;
+        int mid=0;
+        
+        while(high>low)
+        {
+            mid = low + (high-low)/2;
+            
+            if(nums[mid]>=nums[0])
+            {
+                low=mid+1;
+                
+            }else{
+                high=mid;
+            }
+            
+            
+         }
+        
+        
+        pivot=low;
+        
+        //cout<<"pivot is at index "<<pivot<<endl;
+        
+        if(target==nums[pivot])
+        {
+            return pivot;
+        }
+        
+        low=pivot+1;
+        high=nums.size()-1;
+        
+        int ans=-1;
+        ans = binarysearch(nums,low,high,target);
+        
+        if(ans!=-1)
+        {
+            //cout<<"i entered here\n";
+            return ans;
+            
+        }
+        
+        
+        low=0;
+        high=pivot-1;
+       // cout<<"the high for the series is "<<nums[high]<<endl;
+        
+        ans = binarysearch(nums,low,high,target);
+        
+        
+        return ans;
     }
 };
