8aaa1f879efb1cbfe59a9dc10e5a959c2d1e72ad
HimanshuJR <98231288+HimanshuJR@users.noreply.github.com>
2024-06-24 18:04:40 +0530

    Time: 43 ms (70.66%) | Memory: 17.5 MB (11.38%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -1,16 +1,18 @@
- class Solution {
- public:
+class Solution {
+public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size(); // no of rows 
-        int m = matrix[0].size(); // no of elements in a row 
-        for( int i = 0;i<n;i++){
-            for( int j = 0; j<m;j++){
-                if(matrix[i][j] == target){
-                    return true;
-                }
-            }
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int i = 0;
+        int j = n - 1;
+        while (i <= m - 1 && j >= 0) {
+            if (matrix[i][j] == target)
+                return true;
+            if (matrix[i][j] > target)
+                j--;
+            else
+                i++;
         }
         return false;
     }
 };
-    
