84695033adf18d0c2608a91555859e38f05bfe0d
HimanshuJR <98231288+HimanshuJR@users.noreply.github.com>
2024-08-09 17:59:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (59.55%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-    std::vector<int> countBits(int n) {
-        std::vector<int> res(n + 1);  // Create a vector to store the results
-        int offset = 1;  // Initialize offset to the smallest power of 2 (1)
-
-        for (int i = 1; i <= n; i++) {
-            if (i == offset * 2) {  // When i reaches the next power of 2, update offset
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1);
+        int offset = 1;
+        for(int i = 1;i<=n;i++){
+            if( i == offset *2){
                 offset = i;
             }
-            res[i] = res[i - offset] + 1;  // Calculate res[i] using the offset
+            ans[i] = ans[i-offset] +1;
         }
-
-        return res;
+        return ans;
     }
 };
