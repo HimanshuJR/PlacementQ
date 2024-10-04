2cf06f06a8d5fc341e068f10a86ff69c0d1fbe80
HimanshuJR <98231288+HimanshuJR@users.noreply.github.com>
2024-10-04 21:27:28 +0530

    Time: 4 ms (75.04%) | Memory: 10.3 MB (56.01%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        vector<pair<int,string>> valueMap = {
-            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
-            {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
-            {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
-        };
-        string Roman = "";
-        for(auto &[value, symbol] : valueMap) {
-            while(num >= value) {
-                Roman += symbol;
-                num -= value;
-            }
-        }
+        string ones[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        string tens[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        string hundreds[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        string thousands[]= {"", "M", "MM", "MMM"};
+        
+        string Roman =  thousands[num / 1000] + hundreds[(num % 1000) / 100] + tens[(num % 100) / 10] + ones[num % 10];
         return Roman;
     }
 };
